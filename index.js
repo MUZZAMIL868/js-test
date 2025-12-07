2ce9ef66c97d720e3b368bfb6a72b4e622af3bd6
MUZZAMIL HABIB <muzzamilh868@gmail.com>
2025-12-08 00:08:47 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+// interacting with Users and Storing Text
+
+// Topic no:1 Alerts:Showing massege to the user
+
+// Exercise
+
+// 1.
+// alert ("Hello, World!")
+// console.log ("Hello, World!")
+
+// 2.
+// alert ("Muzzamil Habib")
+// console.log ("Muzzamil Habib")
+
+// 3.
+// alert (10)
+// console.log(10)
+
+// 4.
+// var msg = "assalamualikum"
+// alert (msg)
+// console.log (msg)
+
+// 5.
+// var name = "Muzzamil"
+// var msg = "Hello"+ name
+// alert (msg)
+// console.log (msg)
+
+// Topic no:2 Variables For Strings: Declearing and using text data
+
+//Exercise
+
+// 1.
+// var userName="Muzzamil"
+
+// 2.
+// var msg="Welcom"
+// alert (msg)
+
+// 3.
+// var FirstName="Muzzamil"
+// var SacondName="Habib"
+// var FullName= FirstName + SacondName
+// console.log (FullName)
+
+// 4.
+// var quote ="Yesterday is a history Tomorrow is mystery But Today is a Gift Thats Why It is Called Present"
+// console.log(quote)
+
+// 5.
+// var City= "Dubai"
+// alert("Welcome to " + [City])
+
+
+ 
+
+
